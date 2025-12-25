adc8ea8251133657a7bfafdebe42fe0f4b0c8289
Techify Bots <TechifyBots@gmail.com>
2025-12-25 15:08:39 +0530

    Update callback handler to use media editing
    😶‍🌫

--- TechifyBots/callback.py
+++ TechifyBots/callback.py
@@ -1,13 +1,16 @@
+import random
 from pyrogram import Client
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 from Script import text
-from config import ADMIN
+from config import ADMIN,PICS
 
 @Client.on_callback_query()
 async def callback_query_handler(client, query: CallbackQuery):
     if query.data == "start":
-        await query.message.edit_caption(
-            caption=text.START.format(query.from_user.mention),
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.START.format(query.from_user.mention),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('⇆ 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 ⇆', url='https://telegram.me/QuickReactRobot?startgroup=botstart')],
                 [InlineKeyboardButton('ℹ️ 𝖠𝖻𝗈𝗎𝗍', callback_data='about'),
@@ -17,8 +20,10 @@ async def callback_query_handler(client, query: CallbackQuery):
         )
 
     elif query.data == "help":
-        await query.message.edit_caption(
-            caption=text.HELP.format(query.from_user.mention),
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.HELP.format(query.from_user.mention),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('📢 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', url='https://telegram.me/Techifybots'),
                  InlineKeyboardButton('💬 𝖲𝗎𝗉𝗉𝗈𝗋𝗍', url='https://telegram.me/TechifySupport')],
@@ -28,8 +33,10 @@ async def callback_query_handler(client, query: CallbackQuery):
         )
 
     elif query.data == "about":
-        await query.message.edit_caption(
-            caption=text.ABOUT,
+        await query.message.edit_media(
+            InputMediaPhoto(
+                media=random.choice(PICS),
+                caption=text.ABOUT,
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton('👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 👨‍💻', user_id=int(ADMIN))],
                 [InlineKeyboardButton('↩️ 𝖡𝖺𝖼𝗄', callback_data="start"),
@@ -39,4 +46,3 @@ async def callback_query_handler(client, query: CallbackQuery):
 
     elif query.data == "close":
         await query.message.delete()
-
